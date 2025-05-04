da8b213e450bd81a94347b647e9e5715d8809162
Rafael Rillo <rafaelrillocabanne@gmail.com>
2025-05-04 17:05:47 -0300

    added an input search to filter the pokemons

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=120';
 
 interface PokemonListItem {
   name: string;
@@ -16,23 +16,25 @@ interface PokemonDetail {
 }
 
 const listElement = document.querySelector("#pokemon-list") as HTMLUListElement;
+const searchInput = document.querySelector("#search") as HTMLInputElement;
 
 async function fetchPokemonList() {
   const response = await fetch(API_URL)
   const data: PokemonListResponse = await response.json();
-  console.log(data)
 
   for (const pokemon of data.results) {
     const imageUrl = await fetchPokemonDetails(pokemon.url);
 
     const listItem = document.createElement("li");
-    listItem.textContent = pokemon.name;
+    const nameSpan = document.createElement("span");
+    nameSpan.textContent = pokemon.name
 
     const img = document.createElement("img");
     img.src = imageUrl;
     img.alt = pokemon.name;
     img.width = 100;
 
+    listItem.appendChild(nameSpan)
     listItem.appendChild(img);
     listElement.appendChild(listItem);
   }
@@ -44,5 +46,16 @@ async function fetchPokemonDetails(url: string): Promise<string> {
   return data.sprites.front_default
 }
 
-fetchPokemonList();
 
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLocaleLowerCase();
+  const listItems = listElement.querySelectorAll("li");
+  
+  listItems.forEach(item => {
+    const name = item.querySelector("span")?.textContent?.toLocaleLowerCase() || "";
+    const isVisible = name.includes(searchTerm);
+    (item as HTMLLIElement).style.display = isVisible ? "block" : "none";
+  })
+})
+
+fetchPokemonList();
